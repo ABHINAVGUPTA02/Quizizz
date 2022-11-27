5a8e4f64e83df0a8865e6ece6c867b99480aa860
ABHINAVGUPTA02 <abhinav.gupta.ag47@gmail.com>
2022-11-27 15:01:15 +0530

    create the app logic

--- app/src/main/java/com/mini_project/thesilentbell/quizit/MainActivity.java
+++ app/src/main/java/com/mini_project/thesilentbell/quizit/MainActivity.java
@@ -3,12 +3,103 @@ package com.mini_project.thesilentbell.quizit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView questionTV,questionNumberTV;
+    private Button option1Btn,option2Btn,option3Btn,option4Btn;
+    private ArrayList<QuizModal> quizModalArrayList;
+    Random random;
+    int currentScore = 0,questionAttempted = 1,currentPos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        questionTV = findViewById(R.id.idTVQuestion);
+        questionNumberTV = findViewById(R.id.idTVQuestionsAttempted);
+        option1Btn = findViewById(R.id.idBtnOption1);
+        option2Btn = findViewById(R.id.idBtnOption2);
+        option3Btn = findViewById(R.id.idBtnOption3);
+        option4Btn = findViewById(R.id.idBtnOption4);
+        quizModalArrayList = new ArrayList<>();
+        random = new Random();
+        getQuizQuestion(quizModalArrayList);
+        currentPos = random.nextInt(quizModalArrayList.size());
+        setDataToViews(currentPos);
+
+        option1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option1Btn.getText().toString().trim().toLowerCase())){
+                    currentScore++;
+                }
+                questionAttempted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+            }
+        });
+
+        option2Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option2Btn.getText().toString().trim().toLowerCase())){
+                    currentScore++;
+                }
+                questionAttempted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+
+            }
+        });
+
+        option3Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option3Btn.getText().toString().trim().toLowerCase())){
+                    currentScore++;
+                }
+                questionAttempted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+            }
+        });
+
+        option4Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(option4Btn.getText().toString().trim().toLowerCase())){
+                    currentScore++;
+                }
+                questionAttempted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+            }
+        });
+    }
+
+    private void setDataToViews(int currentPos) {
+        questionNumberTV.setText("Questions Attempted: " + questionAttempted);
+
+        questionTV.setText(quizModalArrayList.get(currentPos).getQuestion());
+        option1Btn.setText(quizModalArrayList.get(currentPos).getOption1());
+        option2Btn.setText(quizModalArrayList.get(currentPos).getOption2());
+        option3Btn.setText(quizModalArrayList.get(currentPos).getOption3());
+        option4Btn.setText(quizModalArrayList.get(currentPos).getOption4());
+    }
+
+    private void getQuizQuestion(ArrayList<QuizModal> quizModalArrayList) {
+        quizModalArrayList.add(new QuizModal("Which one is the smallest ocean in the World?","Indian","Pacific","Atlantic","Arctic","Arctic"));
+        quizModalArrayList.add(new QuizModal("Which Country gifted the \'Statue of Liberty\' to USA in 1886?","France","Canada","Brazil","England","France"));
+        quizModalArrayList.add(new QuizModal("Dead Sea is located between which two countries?","Jordan and Sudan","Jordan and Israel","Turkey and UAE","UAE and Egypt","Jordan and Israel"));
+        quizModalArrayList.add(new QuizModal("In which ocean \'Bermuda Triangle\' region is located?","Atlantic","Indian","Pacific","Arctic","Atlantic"));
+        quizModalArrayList.add(new QuizModal("Which country is known as the \'playground of 'Europe\'?","Austria","Holland","Switzerland","Itlay","Switzerland"));
     }
 }
